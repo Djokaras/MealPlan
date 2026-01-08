ba25f26e8a2df320ceeb4c5ae960584bdf2ba837
Djokaras <djordje.lfc@gmail.com>
2026-01-08 03:44:20 +0100

    Icons and no URL bar

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,5 @@
-const CACHE_NAME = 'meal-planner-v1';
+const CACHE_NAME = 'meal-planner-v3'; // Nova verzija za persistence
 
-// Svi fajlovi koje tvoja aplikacija koristi moraju biti ovdje
 const ASSETS = [
 	'./',
 	'./index.html',
@@ -8,38 +7,30 @@ const ASSETS = [
 	'./recepies.js',
 	'./script.js',
 	'./manifest.json',
-	// Ako imaš ikonice, dodaj i njih ovdje
-	'https://cdn-icons-png.flaticon.com/512/3511/3511451.png',
+	'./icons/icon-192.png',
+	'./icons/icon-512.png',
 ];
 
-// Instalacija Service Workera i keširanje resursa
 self.addEventListener('install', (event) => {
 	event.waitUntil(
-		caches.open(CACHE_NAME).then((cache) => {
-			console.log('Caching app assets');
-			return cache.addAll(ASSETS);
-		})
+		caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
 	);
 });
 
-// Strategija: Cache First, fallback to Network
 self.addEventListener('fetch', (event) => {
 	event.respondWith(
 		caches.match(event.request).then((response) => {
-			// Vrati iz keša ako postoji, inače traži na mreži
 			return response || fetch(event.request);
 		})
 	);
 });
 
-// Aktiviranje i čišćenje starih keševa
 self.addEventListener('activate', (event) => {
 	event.waitUntil(
 		caches.keys().then((cacheNames) => {
 			return Promise.all(
 				cacheNames.map((cache) => {
 					if (cache !== CACHE_NAME) {
-						console.log('Clearing old cache');
 						return caches.delete(cache);
 					}
 				})
